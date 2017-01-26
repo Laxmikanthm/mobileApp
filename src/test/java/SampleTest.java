9f43df7d45a3968a71a5c4d22a25ba82924810a7
Nikesh <nikesh@subways-mbp.subwaydai.com>
2017-01-26 14:50:05 -0500

    First commit

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,12 @@
+import base.gui.controls.mobile.generic.MobileLabel;
+import base.pages.mobile.MobileBasePage;
+import base.test.BaseTest;
+import org.apache.struts.mock.TestMockBase;
+
+/**
+ * Created by nikesh on 1/26/17.
+ */
+public class SampleTest extends BaseTest {
+
+
+}
