89f4c0201da61db983095c2078331bfad18ac4fb
GopalBoyina <E002243@cigniti.com>
2017-03-23 19:29:16 +0530

    Updated code for special instructions scenario

---
+++ src/test/java/OrderwithSpecialInstructions.java
@@ -0,0 +1,47 @@
+import Base.SubwayAppBaseTest;
+import enums.Country;
+import org.springframework.test.context.ContextConfiguration;
+import org.testng.annotations.Test;
+import pages.AddCardPage.AddCardPage;
+import pages.ChoosePaymentMethodPage.ChoosePaymentMethodPage;
+import pages.HomePage.HomePage;
+import pages.LandingPage.LandingPage;
+import pages.LoginPage.LoginPage;
+import pages.MenuPage.MenuPage;
+import pages.OrdersPage.OrdersPage;
+import pages.PaymentMethodsPage.PaymentMethodsPage;
+import pages.SubwayPage.SubwayPage;
+import pojos.RemoteOrder;
+import pojos.user.MobileUser;
+
+/**
+ * Created by e002243 on 23-03-2017.
+ */
+
+@ContextConfiguration("classpath:MobileAppBeans.xml")
+public class OrderwithSpecialInstructions extends SubwayAppBaseTest {
+
+    @Test
+    public void OrderSpecialInstructions() throws Exception
+    {
+        int store = 54588;
+        String storeName = "CT Turpike West Southbound 2, Milford, CT 06460";
+        String specialInstructions =this.getClass().getSimpleName();
+        MobileUser mobileUser = new MobileUser(false, Country.UnitedStates, store);
+        mobileUser.registerNewUserHeadless(mobileUser);
+        LandingPage landingPage = goToHomePage(LandingPage.getLandingPageClass(), "MobileApp");
+        LoginPage loginPage = landingPage.gotoLogInPage();
+        HomePage homePage=loginPage.login(mobileUser);
+        MenuPage menuPage = homePage.getUserDetails();
+        SubwayPage subwayPage = menuPage.addPaymentMethods();
+        ChoosePaymentMethodPage choosePaymentMethodPage = subwayPage.addPaymentMethod();
+        AddCardPage addCardPage = choosePaymentMethodPage.ChoosePaymentMethodCreditCard();
+        PaymentMethodsPage paymentMethodsPage= addCardPage.addCardDetails(mobileUser);
+        menuPage.goHome();
+        OrdersPage ordersPage = homePage.findYourSubWay();
+        RemoteOrder remoteOrder = mobileUser.getCart().getRemoteOrder();
+        ordersPage.placeRandomOrderSpecialInstructions(remoteOrder.placeRandomOrderWithSpecificProduct("Sides"), mobileUser, storeName,specialInstructions);
+
+
+    }
+}
