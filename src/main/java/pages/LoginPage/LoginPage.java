9283bf897b9f2733937d737c988d965dde3ef983
e001599 <lavanya.kavuru@cigniti.com>
2017-07-24 20:37:49 +0530

    modifying testcase

--- src/main/java/pages/LoginPage/LoginPage.java
+++ src/main/java/pages/LoginPage/LoginPage.java
@@ -57,7 +57,7 @@ public abstract class LoginPage<T extends AppiumDriver> extends MobileBasePage {
     public HomePage login(MobileUser mobileUser) throws Exception {
         try {
             try {
-                Thread.sleep(3000);
+                Thread.sleep(6000);
                 driver.findElementByXPath("//android.widget.EditText[@resource-id='custom-signInName']");
 
             }
